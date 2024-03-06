a76de431f91ed2b8b7268e59263dc90d608e5419
widiaaas <160681667+widiaaas@users.noreply.github.com>
2024-03-06 10:03:48 +0700

    Delete Praktikum 3/src directory

--- Praktikum 3/src/AngkaSialException.java
+++
@@ -1,8 +0,0 @@
-/*
-* Deskripsi : Ekspresi buatan sendiri, menolak masukkan angka 13
-* */
-public class AngkaSialException extends Exception {
-    public AngkaSialException(){
-        super("jangan masukkan angka 13 karena angka sial");
-    }
-}
